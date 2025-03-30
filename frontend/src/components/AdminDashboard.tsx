ca4470ac0cf59a93bce6749d73b0a7b12b94b441
Dev <khero365@gmail.com>
2025-03-30 15:56:04 +0530

    Final Deploy

--- frontend/src/components/AdminDashboard.tsx
+++ frontend/src/components/AdminDashboard.tsx
@@ -57,7 +57,7 @@ export default function ParkingDashboard() {
     async function fetchParkingData() {
       try {
         // const response = await fetch("http://parkingdashboard_srpproject.railway.internal/api/parkingData");
-        const response = await fetch("http://localhost:3000/api/parkingData");
+        const response = await fetch("https://parking-backend-ohmo.onrender.com/api/parkingData");
         // const response = await fetch('$(import.meta.env.VITE_API_URL)/api/parkingData');
         const data = await response.json();
         setParkingData(data);
@@ -72,7 +72,7 @@ export default function ParkingDashboard() {
     async function fetchOccupancyData() {
       try {
         // const response = await fetch("http://parkingdashboard_srpproject.railway.internal/api/occupancy");
-        const response = await fetch("http://localhost:3000/api/occupancy");
+        const response = await fetch("https://parking-backend-ohmo.onrender.com/api/occupancy");
         const data = await response.json();
         setOccupancyData(data);
 
